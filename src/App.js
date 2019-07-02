cbfd3c104d4624b06e8df190479461e4f5233dad
egboamara <egboamara31@gmail.com>
2019-07-02 12:21:13 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import Clock from './components/clock'
-
+import Clock from './components/clock';
 import './App.css';
 
 
-
-
-
-
-constructor
 class App extends React.Component{
- constructor(props){
-   super(props)
-   this.state ={
-     date:new Date(),
-     name:'Mekarosi',
-     country: 'Nigeria'
-   }
- }
  render(){
    return(
 
-     <div class='App-header'><Clock></Clock></div>
+     <div class='App-header'>
+       <Clock></Clock>
+       </div>
 
      //<h1>I am queen and the time is {this.state.date.toDateString()}</h1>
    )
  }
-}
 
+}
 export default App;
